fa01a039999d3d0a719efbab27473a817c4dcba3
Juan Cruz <42083637+jc23729@users.noreply.github.com>
2021-09-24 17:58:13 +0000

    update

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-// Create the below linked list:
-// myLinkedList = {
-//   head: {
-//     value: 10
-//     next: {
-//       value: 5
-//       next: {
-//         value: 16
-//         next: null
-//       }
-//     }
-//   }
-// };
+// 10-->5-->16 
 
+class LinkedList {
+  constructor(value) {
+    // we need to keep track of the head 
+    this.head = {
+      //this is the value that we pass in which 10. (let myLinkedList = new LinkedList(10);)
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    //Code here
+  }
+}
+
+// Im gonna instantiate or run or class LinkedListConstructor and our value is going to be 10
+let myLinkedList = new LinkedList(10);
+// myLinkedList.append(5);
+// myLinkedList.append(16);
+
+
+
+// UNCOMMENTED CODE 
 // class LinkedList {
 //   constructor(value) {
 //     this.head = {
@@ -25,25 +37,3 @@
 //     //Code here
 //   }
 // }
-
-// let myLinkedList = new LinkedList(10);
-// myLinkedList.append(5);
-// myLinkedList.append(16);
-
-// 10-->5-->16 
-class LinkedList {
-  constructor(value) {
-    this.head = {
-      //All you need is value of the property
-      // the value will change but the pointer will still point to the same item
-      value: 10,
-      // and the pointer to the next node 
-      next: 5,
-    };
-    this.tail = this.head;
-    this.length = 1;
-  }
-  append(value) {
-    //Code here
-  }
-}
