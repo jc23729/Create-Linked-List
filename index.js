3c5f8dbcd9bc157bc34eca5c6c233e49a46023d2
Juan Cruz <42083637+jc23729@users.noreply.github.com>
2021-09-24 17:39:32 +0000

    update

--- index.js
+++ index.js
@@ -12,10 +12,31 @@
 //   }
 // };
 
+// class LinkedList {
+//   constructor(value) {
+//     this.head = {
+//       value: value,
+//       next: null
+//     };
+//     this.tail = this.head;
+//     this.length = 1;
+//   }
+//   append(value) {
+//     //Code here
+//   }
+// }
+
+// let myLinkedList = new LinkedList(10);
+// myLinkedList.append(5);
+// myLinkedList.append(16);
+
+
 class LinkedList {
   constructor(value) {
     this.head = {
+      //All you need is value of the property
       value: value,
+      // and the pointer
       next: null
     };
     this.tail = this.head;
@@ -25,10 +46,3 @@ class LinkedList {
     //Code here
   }
 }
-
-let myLinkedList = new LinkedList(10);
-myLinkedList.append(5);
-myLinkedList.append(16);
-
-
-
